5cff05de62af451ece859a0b56c0d0a11f4d02b4
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-10-24 17:21:39 +0800

    add wan module for web3

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 exports.rlp = require("./rlp.js")
 exports.ethereumUtil = require("./ethereumjs-util.js")
 exports.ethereumTx = require("./ethereumjs-tx.js")
-
+exports.web3Wan = require("./web3_wan.js");
